3d5a53aa0fbfaf968d9903fa19e97e22066ed11d
Lallluk <lallluklaluk@gmail.com>
2026-09-17 15:44:28 +0200

    Initialize local SQLite database

--- SocialDashboard/App.xaml.cs
+++ SocialDashboard/App.xaml.cs
@@ -15,6 +15,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using SocialDashboard.Data;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,6 +36,9 @@ namespace SocialDashboard
         public App()
         {
             InitializeComponent();
+
+            using AppDbContext database = new();
+            database.Database.EnsureCreated();
         }
 
         /// <summary>
---
+++ SocialDashboard/Data/AppDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialDashboard.Models;
+using System;
+using System.IO;
+
+namespace SocialDashboard.Data;
+
+public sealed class AppDbContext : DbContext
+{
+    public DbSet<VideoAsset> Videos => Set<VideoAsset>();
+
+    public DbSet<YouTubeAccount> YouTubeAccounts => Set<YouTubeAccount>();
+
+    public DbSet<YouTubeUpload> YouTubeUploads => Set<YouTubeUpload>();
+
+    public DbSet<YouTubeStatistics> YouTubeStatistics => Set<YouTubeStatistics>();
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        string appDataFolder = Path.Combine(
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData),
+            "SocialDashboard");
+
+        Directory.CreateDirectory(appDataFolder);
+
+        string databasePath = Path.Combine(
+            appDataFolder,
+            "socialdashboard.db");
+
+        optionsBuilder.UseSqlite($"Data Source={databasePath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<VideoAsset>()
+            .Property(video => video.LocalFilePath)
+            .IsRequired();
+
+        modelBuilder.Entity<VideoAsset>()
+            .Property(video => video.FileName)
+            .IsRequired();
+
+        modelBuilder.Entity<YouTubeAccount>()
+            .Property(account => account.ChannelId)
+            .IsRequired();
+
+        modelBuilder.Entity<YouTubeUpload>()
+            .Property(upload => upload.Title)
+            .IsRequired();
+    }
+}
